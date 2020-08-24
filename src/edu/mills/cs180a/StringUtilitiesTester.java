2f9942245b9a6de5b733e50791902b7830d82433
Ellen Spertus <spertus@mills.edu>
2020-08-23 18:19:14 -0700

    Rename test file

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class SubstringCheckerTester {
+class StringUtilitiesTester {
 	@Test
 	void testNullInputs() {
 		assertThrows(
