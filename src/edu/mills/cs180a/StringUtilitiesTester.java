c1074aa490bbb93406c5719181d57e058dfaacb7
Ellen Spertus <spertus@mills.edu>
2020-09-25 09:29:36 -0700

    Prepare for distribution

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -1,36 +1,20 @@
 package edu.mills.cs180a;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class StringUtilitiesTester {
-	@Test
-	void testNullInputs() {
-		assertThrows(
-				NullPointerException.class,
-				() -> StringUtilities.isSubstring(null, "foo"));
-		assertThrows(
-				NullPointerException.class,
-				() -> StringUtilities.isSubstring("foo", null));
-		assertThrows(
-				NullPointerException.class,
-				() -> StringUtilities.isSubstring(null, null));
-	}
-	
-	@Test
-	void testEmptyStrings() {
-		assertTrue(StringUtilities.isSubstring("", "foo"));
-		// TODO: write rest
-	}
-	
-	@Test
-	void testMatchingStrings() {
-		// TODO: write
-	}
-	
-	@Test
-	void testNotMatchingStrings() {
-		// TODO: write
-	}
+  @Test
+  void isSubstring_False_null() {
+    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, "foo"));
+    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring("foo", null));
+    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, null));
+  }
+
+  @Test
+  void isSubstring_False_EmptyString() {
+    assertTrue(StringUtilities.isSubstring("", "foo"));
+    // TODO: write rest
+  }
 }
